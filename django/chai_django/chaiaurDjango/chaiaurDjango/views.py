e11bf83236a75a5e54fdaf4d3341c4e2c61353be
Himani <himani.kapoor@matrixhive.com>
2025-12-19 19:29:44 +0530

    Django concept (ongoing)

---
+++ django/chai_django/chaiaurDjango/chaiaurDjango/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("Hello, World. You are at chai aur Django Home Page")
+    return render(request, 'index.html')
+
+def about(request):
+    return HttpResponse("Hello, World. You are at chai aur Django About Page")
+
+def contact(request):
+    return HttpResponse("Hello, World. You are at chai aur Django Contact Page")
